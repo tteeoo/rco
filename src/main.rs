a90ea497b656419ec30d199d4596bd52c0626eb0
Theo Henson <theodorehenson@protonmail.com>
2020-02-12 22:46:30 -0500

    commented code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+/* Copyright (c) 2020 Theo Henson, MIT License
+ *
+ * A remake of my program comma, now made in rust
+ *
+*/
+
 mod error_exits;
 mod parse;
 
@@ -7,14 +13,23 @@ mod parse;
 }*/
 
 fn main() {
+    // Atempts to get the amount of objects (really just the amount of lines in the objects file)
     let obj_count = match parse::line_count("/home/theo/Repos/rco/src/objects.csv") {
+
+        // If there was an error, it sets the object count to 0, if not it sets obj_count correctly
         Err(_why) => 0,
         Ok(x) => x,
     };
+
+    // If obj_count is 0, an error is thrown, exiting the program
+    // This is caused either by there being no objects (such that the program will not work)
+    // Or because there was an error reading the file, as described above
     if obj_count == 0 {
         error_exits::obj_file();
     }
-    println!("{:?}", obj_count);
+
+    // Fore debug purposes, prints the obj_count
+    println!("{}", obj_count);
 
     //parse::go();
 }
--- src/parse.rs
+++ src/parse.rs
@@ -12,10 +12,16 @@ use std::io::prelude::*;
     Ok(())
 }*/
 
-pub fn line_count(filename: &str) -> std::io::Result<isize> {
-    let mut count: isize = 0;
+// Counts the amount of lines in given file
+// This is useful for determining the needed size of an array to hold all the objects
+pub fn line_count(filename: &str) -> std::io::Result<i32> {
+    let mut count = 0;
+
+    // Opens file
     let file = File::open(filename)?;
     let reader = BufReader::new(file);
+
+    // Iterates through each line and counts how many there are
     for _line in reader.lines() {
         count += 1;
     }
